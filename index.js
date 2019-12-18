0106fe2006ad20bb1091cd5c55255d1ca7884670
Katie Belanger <belangerkat@gmail.com>
2019-12-18 19:17:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,11 +26,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length - 1)
-  return kittens
+  return kittens.slice(0, kittens.length - 1)
 }
 
 function removeFirstKitten() {
-  kittens.slice(1)
-  return kittens
+  return kittens.slice(1)
 }
